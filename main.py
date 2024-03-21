3679a1dd90734249b4d6a244334e8ffc8ca86d03
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-20 19:35:49 -0700

    edited last statement to be outside of loop

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ usr_str = input("Hey how's it going? I am pretty good, you?\n")
 while usr_str != "stop copying me":
     print(usr_str)
     usr_str = input()
-    if usr_str == "stop copying me":
-        print("UGH FINE YOU WIN")
-        break
+print("UGH FINE YOU WIN")
+
 
