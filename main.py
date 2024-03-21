0bec673df11bafb3541fed021dbe421d1f61d81a
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-20 19:28:00 -0700

    added input inside loop, break statement

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-usr_str = input("Hey how's it going? I am pretty good, you?")
+usr_str = None
 
 while usr_str != "stop copying me":
+    usr_str = input("Hey how's it going? I am pretty good, you?\n")
+
+    if usr_str == "stop copying me":
+        print("UGH FINE YOU WIN")
+        break
     print(usr_str)
+
