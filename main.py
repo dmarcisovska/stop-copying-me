97caff712774f8dc541b6209e1139b92b3783fc6
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 18:21:35 -0700

    reverted code back - accidently put another project into code

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
-import random
-keep_playing = True
-
-while keep_playing:
-    ran_num = random.randint(1, 10)
-    guess = int(input("Guess a number between 1 and 10: "))
-    if guess == ran_num:
-        want_continue = input("Do you want to keep playing? y/n ")
-        if want_continue == "y":
-            keep_playing = True
-        else:
-            keep_playing = False
+usr_str = input("Hey how's it going? I am pretty good, you?\n")
+while usr_str != "stop copying me":
+    print(usr_str)
+    usr_str = input()
+print("UGH FINE YOU WIN")
 
 
 
