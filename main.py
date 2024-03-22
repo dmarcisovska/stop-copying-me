cc756443287fa10aa059e0fc55dc75380e4dd4e2
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 17:57:03 -0700

    generating random numbers

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-
-usr_str = input("Hey how's it going? I am pretty good, you?\n")
-while usr_str != "stop copying me":
-    print(usr_str)
-    usr_str = input()
-print("UGH FINE YOU WIN")
-
+import random
+ran_num = random.randint(1,10)
 
