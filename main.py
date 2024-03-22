db12aa719dde1870695d2f16044d5c3f6d637e89
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 17:59:37 -0700

    testing breaking out of loop

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 import random
-ran_num = random.randint(1,10)
+# ran_num = random.randint(1,10)
+keep_playing = True
+
+while keep_playing:
+    ran_num = random.randint(1, 10)
+    print(ran_num)
+    guess = input("Guess a number between 1 and 10: ")
+
+    if guess == ran_num:
+        keep_playing = False
 
